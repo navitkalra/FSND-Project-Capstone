7baff75dd5f9e4163f80a6dacdfcadd9ca78e5fa
navitkalra <61940543+navitkalra@users.noreply.github.com>
2020-06-12 23:39:13 +0530

    Initial Creation

---
+++ models.py
@@ -0,0 +1,101 @@
+import os
+from sqlalchemy import Column, Integer, String, create_engine, Date
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+database_name = os.environ['DATABASE_NAME']
+db_user = os.environ['DATABASE_USER']
+db_password = os.environ['DATABASE_PWD']
+
+database_path = "postgres://{}:{}@{}/{}".format(
+    db_user, db_password, 'localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+'''
+setup_db(app) binds a flask application and a SQLAlchemy service
+
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Actors Table & Model
+'''
+
+class Actors(db.Model):  
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+    age = Column(Integer)
+
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name' : self.name,
+        'gender': self.gender,
+        'age': self.age
+        }
+
+
+'''
+Movies Table & Model
+'''
+
+
+class Movies(db.Model):  
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+
+    def __init__(self, title, release_date) :
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title' : self.title,
+        'release_date': self.release_date
+        }
---
+++ setup.sh
@@ -0,0 +1,6 @@
+export AUTH0_DOMAIN='fsndkalra1.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='cagency'
+export DATABASE_NAME='castagency'
+export DATABASE_USER='postgres'
+export DATABASE_PWD='12345678'
