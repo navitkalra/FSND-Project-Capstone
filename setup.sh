f3f27e56a4854f6b335658d63dd19ff969a52284
navitkalra <61940543+navitkalra@users.noreply.github.com>
2020-06-16 03:43:58 +0530

    Uploaded Corrected Setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 export AUTH0_DOMAIN='fsndkalra1.auth0.com'
-export ALGORITHMS=['RS256']
+export ALGORITHMS='RS256'
 export API_AUDIENCE='cagency'
+
+#local database url
 export DATABASE_URL='postgres://postgres:12345678@localhost:5432/castagency'
+
+#heroku database url
+export DATABASE_URL='postgres://hkyeustnkszunx:522a8ad98d5a8a8d9a6cc22ba930469e35f6b714757e032dc754009dc31bccf9@ec2-3-216-129-140.compute-1.amazonaws.com:5432/de37vpaq4ak84t'
